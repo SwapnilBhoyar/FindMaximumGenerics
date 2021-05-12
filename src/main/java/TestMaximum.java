5e09c1e53944a7418a62470e9ea08bf74f51782f
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-05-13 04:37:13 +0530

    REFACTORED2: to generic class

---
+++ src/main/java/TestMaximum.java
@@ -0,0 +1,25 @@
+public class TestMaximum<E extends Comparable<E>> {
+    E firstValue, secondValue, thirdValue;
+    TestMaximum(E firstValue, E secondValue, E thirdValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.thirdValue = thirdValue;
+    }
+
+    public TestMaximum() {
+    }
+
+    public E testMaximum() {
+        return testMaximum(firstValue, secondValue, thirdValue);
+    }
+    public static <E extends Comparable<E>> E testMaximum(E firstValue, E secondValue, E thirdValue) {
+        E maxValue = firstValue;
+        if (secondValue.compareTo(maxValue) > 0) {
+            maxValue = secondValue;
+        }
+        if (thirdValue.compareTo(maxValue) > 0) {
+            maxValue = thirdValue;
+        }
+        return maxValue;
+    }
+}
---
+++ src/test/java/FindMaximumTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class FindMaximumTest {
+    TestMaximum testMaxObj = new TestMaximum();
+
+    @Test
+    void ifIntegerNumberOneIsMaxReturnTrue() {
+        TestMaximum<Integer> testMaximumInteger = new TestMaximum<>(100, 50, 25);
+        Assertions.assertEquals(100, testMaximumInteger.testMaximum());
+    }
+
+    @Test
+    void ifIntegerNumberTwoIsMaxReturnTrue() {
+        TestMaximum<Integer> testMaximumInteger = new TestMaximum<>(50, 100, 25);
+        Assertions.assertEquals(100, testMaximumInteger.testMaximum());
+    }
+
+    @Test
+    void ifIntegerNumberThreeIsMaxReturnTrue() {
+        TestMaximum<Integer> testMaximumInteger = new TestMaximum<>(25, 50, 100);
+        Assertions.assertEquals(100, testMaximumInteger.testMaximum());
+    }
+
+    @Test
+    void ifFloatNumberOneIsMaxReturnTrue() {
+        TestMaximum<Float> testMaximumInteger = new TestMaximum<>(10.0F, 5.0F, 2.5F);
+        Assertions.assertEquals(10.0F, testMaximumInteger.testMaximum());
+    }
+    @Test
+    void ifFloatNumberTwoIsMaxReturnTrue() {
+        TestMaximum<Float> testMaximumInteger = new TestMaximum<>(5.0F, 10.0F, 2.5F);
+        Assertions.assertEquals(10.0F, testMaximumInteger.testMaximum());
+    }
+    @Test
+    void ifFloatNumberThreeIsMaxReturnTrue() {
+        TestMaximum<Float> testMaximumInteger = new TestMaximum<>(2.5F, 5.0F, 10.0F);
+        Assertions.assertEquals(10.0F, testMaximumInteger.testMaximum());
+    }
+    @Test
+    void ifStringOneIsMaxReturnTrue() {
+        TestMaximum<String> testMaximumInteger = new TestMaximum<>("banana", "peach", "apple");
+        Assertions.assertEquals("banana", testMaximumInteger.testMaximum());
+    }
+
+    @Test
+    void ifStringTwoIsMaxReturnTrue() {
+        TestMaximum<String> testMaximumInteger = new TestMaximum<>("apple", "banana", "peach");
+        Assertions.assertEquals("banana", testMaximumInteger.testMaximum());
+    }
+    @Test
+    void ifStringThreeIsMaxReturnTrue() {
+        TestMaximum<String> testMaximumInteger = new TestMaximum<>("peach", "apple", "banana");
+        Assertions.assertEquals("banana", testMaximumInteger.testMaximum());
+    }
+}
